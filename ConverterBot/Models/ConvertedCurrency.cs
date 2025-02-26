083a7321f8f200778b83434fc8f2c151094daa39
dnosur <45685967+dnosur@users.noreply.github.com>
2025-02-26 11:40:39 +0200

    feat(ConvertedCurrency): add ConvertedCurrency table model

---
+++ ConverterBot/Models/ConvertedCurrency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterBot.Models
+{
+    internal class ConvertedCurrency
+    {
+        public float value { get; set; }
+    }
+}
